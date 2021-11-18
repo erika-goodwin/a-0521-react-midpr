d2a78e914aaeaa38d32af98243111c0c70545b03
erika-goodwin <shellvoerika@gmail.com>
2021-11-18 14:17:36 -0800

    added https

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -30,7 +30,7 @@ export default class WeatherApp extends Component {
 
   // fetch Data ===================================
   getCityWeather = (city) => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`;
     //fetch
     fetch(url)
       .then((res) => res.json())
